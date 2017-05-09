a2abdf00a79fc898f5938834b81bb90835c178b4
John Nolette <john@neetgroup.net>
2017-05-09 13:43:09 -0400

    Added loading splash.

--- app/app.php
+++ app/app.php
@@ -4,6 +4,12 @@
 
 <!-- App entry point -->
 <app>
+  <div class="loading loading-wrapper">
+    <div class="loading loading-container">
+      <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
   <app-header></app-header>
   <router-view></router-view>
   <app-footer></app-footer>
@@ -21,13 +27,14 @@
 
 <!-- App init -->
 <script type="text/javascript">
-  $.getJSON('app/config/i18n.json', function(data) {
+  $.getJSON('app/config/i18.json', function(data) {
+    $('div.loading').addClass('animated zoomOut'); // # hide loading screen
     Vue.use(VueLocalize, {
       lang_default: 'en',
       localizations: data
-    });
+    }); // # install localize plugin
     const App = new Vue({
       router
     }).$mount('app');
-  });
+  }); // # instantiate app
 </script>
