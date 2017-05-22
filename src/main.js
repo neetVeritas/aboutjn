12242b353bf5a63bf18436631fab2c5d64f7fda0
John Nolette <JNolette@Transparent.com>
2017-05-21 22:50:08 -0400

    Added dev dependencies and webpack build configuration.

--- src/main.js
+++ src/main.js
@@ -3,15 +3,24 @@ import VueLocalize from 'v-localize'
 
 import App from './app.vue'
 
-Vue.use()
+Vue.use(VueLocalize,
+{
+
+});  // # install localization
 
 const App = new Vue({
-  router
+  router,
+  store,
   components: {
     Header,
     Footer,
     Loading,
     Author,
     Navbar
+  },
+  created: function() {
+    particlesJS.load('view', `app/config/particles.json?t=${new Date().getTime().toString()}`, function() {
+      console.log('Particles have been loaded successfully.');
+    });
   }
 }).$mount('app');
