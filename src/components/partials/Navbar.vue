3a4cd9bc21fd3af96c1aeec90567824689a5efca
John Nolette <john@neetgroup.net>
2017-12-29 01:33:21 -0500

    Added mobile navbar

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -1,34 +1,38 @@
 <template>
   <nav>
-    <div class="nav-left">
-      <ul class="nav-menu">
-        <li>
-          <router-link to="/home" v-localize="{item: 'header.nav.home'}"></router-link>
-        </li>
-        <li>
-          <router-link to="/blog" v-localize="{item: 'header.nav.blog'}"></router-link>
-        </li>
-        <li>
-          <router-link to="/about" v-localize="{item: 'header.nav.about'}"></router-link>
-        </li>
-        <li>
-          <router-link to="/resume" v-localize="{item: 'header.nav.resume'}"></router-link>
-        </li>
-      </ul>
+    <div class="nav-desktop">
+      <div class="nav-left">
+        <ul class="nav-menu">
+          <li>
+            <router-link to="/home" v-localize="{item: 'header.nav.home'}"></router-link>
+          </li>
+          <li>
+            <router-link to="/blog" v-localize="{item: 'header.nav.blog'}"></router-link>
+          </li>
+          <li>
+            <router-link to="/about" v-localize="{item: 'header.nav.about'}"></router-link>
+          </li>
+          <li>
+            <router-link to="/resume" v-localize="{item: 'header.nav.resume'}"></router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="nav-right">
+        <ul class="nav-menu">
+          <li class="nav-icon nav-icon-social">
+            <a title="Twitter" href="https://twitter.com/neet_jn">
+              <i class="fa fa-twitter-square"></i>
+            </a>
+          </li>
+          <li class="nav-icon nav-icon-social">
+            <a title="Github" href="https://github.com/neetVeritas">
+              <i class="fa fa-github-square"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="nav-right">
-      <ul class="nav-menu">
-        <li class="nav-icon nav-icon-social">
-          <a title="Twitter" href="https://twitter.com/neet_jn">
-            <i class="fa fa-twitter-square"></i>
-          </a>
-        </li>
-        <li class="nav-icon nav-icon-social">
-          <a title="Github" href="https://github.com/neetVeritas">
-            <i class="fa fa-github-square"></i>
-          </a>
-        </li>
-      </ul>
+    <div class="nav-mobile">
     </div>
   </nav>
 </template>
