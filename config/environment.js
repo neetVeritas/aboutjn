a8e0189bd0b84a3d34b6036eef05897bec5f07ad
John Nolette <john@neetgroup.net>
2018-05-14 17:32:12 -0500

    Cleaned up home page, blog

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 module.exports = function(environment) {
   let ENV = {
@@ -24,7 +24,7 @@ module.exports = function(environment) {
       'Lobster:300',
       'Oswald:300'
     ]
-  };
+  }
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
@@ -36,19 +36,19 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.locationType = 'none';
+    ENV.locationType = 'none'
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
 
-    ENV.APP.rootElement = '#ember-testing';
-    ENV.APP.autoboot = false;
+    ENV.APP.rootElement = '#ember-testing'
+    ENV.APP.autoboot = false
   }
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
 
-  return ENV;
-};
+  return ENV
+}
