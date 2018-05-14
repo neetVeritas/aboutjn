8ffc3b60c4f1f3fb0a9dfdb2efc91096afa680a8
John Nolette <john@neetgroup.net>
2018-05-13 22:14:53 -0500

    Fixed footer, added basic styles. Fixed icons

--- app/components/nav-header.js
+++ app/components/nav-header.js
@@ -1,4 +1,15 @@
+import Ember from 'ember'
+import anime from 'ember-animejs'
 import Component from '@ember/component'
 
 export default Component.extend({
+  didInsertElement() {
+    Ember.$(window).on('load', () => {
+      anime({
+        targets: '.news-banner',
+        points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+        easing: 'easeInOutExpo'
+      })
+    })
+  }
 })
